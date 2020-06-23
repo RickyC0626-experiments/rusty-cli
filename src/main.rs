f594db9c50230ba5ea3590842c10c7d70fb64897
RickyC0626 <rickychon99@gmail.com>
2020-06-23 19:14:09 -0400

    Add simple CLI struct with two arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use structopt::StructOpt;
+
+// Search for a pattern in a file and display the lines that contain it
+#[derive(StructOpt)]
+#[derive(Debug)]
+struct CLI
+{
+    // The pattern to look for
+    pattern: String,
+    // The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() 
 {
-    println!("Hello, world!");
+    let args = CLI::from_args();
+    println!("rusty-cli: {:?} {:?}", args.pattern, args.path);
 }
